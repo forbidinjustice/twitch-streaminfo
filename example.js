e1030d3faa5b152f2041d1fe75308ff61f56c909
DBKynd <db@dbkynd.com>
2016-06-28 22:29:10 -0600

    Connect to Twitch
    Added tmi.js library and handled Twitch status update server side

--- example.js
+++ example.js
@@ -2,7 +2,14 @@
 var app = require('./lib');
 
 var options = {
-    port: 3000
+    port: 3000, //The port this script will bind to
+    page_title: "Stream Info", //Browser page's Title (Tab Title)
+    twitch: {
+        nick: "", //Your Twitch Username
+        oauth: "", //Your oauth token. Generate at: http://www.twitchapps.com/tmi/
+        channel: "", //The twitch channel to monitor
+        client_id: "" //A Twitch app's client ID to avoid any rate limits - Optional
+    }
 };
 
 app = new app(options);
