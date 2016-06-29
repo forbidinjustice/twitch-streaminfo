138a7d04f351a09c32cbe83bfbf2d91d970b1103
DBKynd <db@dbkynd.com>
2016-06-29 10:06:46 -0600

    Altered comments

--- example.js
+++ example.js
@@ -3,13 +3,14 @@ var app = require('./lib');
 
 var options = {
     port: 3000, //The port this script will bind to
-    page_title: "Stream Info", //Browser page's Title (Tab Title)
+    page_title: "Stream Info", //This sets the browser page's Title (Tab Title)
     twitch: {
         nick: "", //Your Twitch Username
         oauth: "", //Your oauth token. Generate at: http://www.twitchapps.com/tmi/
         channel: "" //The twitch channel to monitor
     },
-    mod_bot_name: "NightBot" //This sets the name in the page's status bar and is what we monitor for mod/unmod messages to see if it's in-channel
+    //This sets the bot name in the page's status bar and is what we monitor for mod/unmod messages to see if it's in-channel
+    mod_bot_name: "NightBot"
 };
 
 app = new app(options);
